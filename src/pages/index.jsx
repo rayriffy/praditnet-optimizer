4937b514949a705d05445fef5450c41e82effa67
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-26 08:33:09 +0900

    feat: intitial c

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default () => {
+  return (
+    <Image
+      src="https://praditnet-cdn.rayriffy.com/chunithm/character/deka/240.png"
+      width={1080}
+      height={1080}
+    />
+  )
+}
