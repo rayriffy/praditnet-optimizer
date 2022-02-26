43ad2a1c33694482143ab48edef804b6af33dbdf
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-26 09:06:49 +0900

    chore: caching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   images: {
-    formats: ['image/webp'],
-    domains: ['praditnet-cdn.rayriffy.com']
-  }
-}
+    formats: ["image/webp"],
+    domains: ["praditnet-cdn.rayriffy.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/_ipx/:path*",
+        headers: [
+          {
+            key: "cache-control",
+            value:
+              "public, max-age=2592000",
+          },
+        ],
+      },
+    ];
+  },
+};
