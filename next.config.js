02bae21c0aa523494b690512b263f9f9c5eaefbe
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-26 09:18:46 +0900

    chore: attempt to use toml override headers

--- next.config.js
+++ next.config.js
@@ -3,18 +3,4 @@ module.exports = {
     formats: ["image/webp"],
     domains: ["praditnet-cdn.rayriffy.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/_ipx/:path*",
-        headers: [
-          {
-            key: "cache-control",
-            value:
-              "public, max-age=2592000",
-          },
-        ],
-      },
-    ];
-  },
 };
